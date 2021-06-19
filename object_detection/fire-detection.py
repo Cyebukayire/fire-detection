b4f9ba227e5db6b286ca46237d6d0e7a8f20a86b
Cyebukayire <peacecyebukayire@gmail.com>
2021-06-19 14:05:22 +0200

    fire detection

--- object_detection/fire-detection.py
+++ object_detection/fire-detection.py
@@ -74,6 +74,10 @@ with detection_graph.as_default():
 # In[7]:
 
 
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
 
 # # Detection
 
